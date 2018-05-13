5ef74540ebb0d07d833db10f4a47d6d86a75932e
Frederic JOSEPH <fredjoseph@users.noreply.github.com>
2018-05-13 12:56:30 +0200

    refactoring : add an utility method -> colored_text

--- powerline_prompt.lua
+++ powerline_prompt.lua
@@ -46,6 +46,10 @@ local function get_git_status()
     return true
 end
 
+local function colored_text(text, foreground, background, bold)
+    return color.set_color(foreground, background, bold)..text
+end
+
 -- Specific script constants
 local PROMPT_FULL = "full"
 local PROMPT_FOLDER = "folder"
@@ -53,6 +57,7 @@ local ARROW_SYMBOL = ""
 local BRANCH_SYMBOL = ""
 local PROMPT_END_CHAR = "λ"
 local PROMPT_ADMIN_CHAR = "⚡"
+local RESET_SEQ = "\x1b[0m"
 
 local PROMPT_PATH_TYPES = createTable(PROMPT_FULL, PROMPT_FOLDER)
 local TRUE_OR_FALSE = createTable(true, false)
@@ -75,14 +80,14 @@ function reset_prompt_filter()
     local prompt_lhs = "{env}{lamb} \x1b[0m"
     clink.prompt.value = prompt_header .. "\n" .. prompt_lhs
     
-    clink.prompt.value = string.gsub(clink.prompt.value, "{lamb}", color.set_color(color.GREEN,color.BLACK, color.BOLD)..PROMPT_END_CHAR)
+    clink.prompt.value = string.gsub(clink.prompt.value, "{lamb}", colored_text(PROMPT_END_CHAR, color.GREEN, color.BLACK, color.BOLD))
 end
 
 function admin_prompt_filter()
     if settings.display_admin then
         local _,_,ret = os.execute("net session 1>nul 2>nul")
         if ret == 0 then
-            clink.prompt.value = string.gsub(clink.prompt.value, "{admin}", color.set_color(color.YELLOW,color.BLACK, color.BOLD)..PROMPT_ADMIN_CHAR.."\x1b[0m ")
+            clink.prompt.value = string.gsub(clink.prompt.value, "{admin}", colored_text(PROMPT_ADMIN_CHAR, color.YELLOW, color.BLACK, color.BOLD)..RESET_SEQ.." ")
             return false;
         end
     end
@@ -94,9 +99,8 @@ function user_prompt_filter()
     if settings.display_user then
         local username = clink.get_env("USERNAME")
         local host = clink.get_env("COMPUTERNAME")
-        local user_color = color.set_color(color.WHITE, color.BLACK)
         
-        clink.prompt.value = string.gsub(clink.prompt.value, "{user}", user_color..username.."@"..host)
+        clink.prompt.value = string.gsub(clink.prompt.value, "{user}", colored_text(username.."@"..host, color.WHITE, color.BLACK))
         return false
     end
     
@@ -121,7 +125,7 @@ function cwd_prompt_filter()
         cwd = string.gsub(cwd, home_pattern, "~")
     end
     
-    clink.prompt.value = string.gsub(clink.prompt.value, "{cwd}", color.set_color(color.WHITE, color.BLUE)..cwd)
+    clink.prompt.value = string.gsub(clink.prompt.value, "{cwd}", colored_text(cwd, color.WHITE, color.BLUE))
 end
 
 function git_prompt_filter()
@@ -138,14 +142,9 @@ function git_prompt_filter()
         local branch = gitutil.get_git_branch(git_dir)
         if branch then
             -- Has branch => now figure out status
-            local branch_color = ""
-            if get_git_status() then
-                branch_color = color.set_color(color.BLACK, color.GREEN)
-            else
-                branch_color = color.set_color(color.BLACK, color.YELLOW)
-            end
+            local background_color = get_git_status() and color.GREEN or color.YELLOW
             
-            clink.prompt.value = string.gsub(clink.prompt.value, "{git}", branch_color..BRANCH_SYMBOL.." "..branch)
+            clink.prompt.value = string.gsub(clink.prompt.value, "{git}", colored_text(BRANCH_SYMBOL.." "..branch, color.BLACK, background_color))
             return false
         end
     end
@@ -166,7 +165,7 @@ function env_prompt_filter()
         end
     end
     
-    clink.prompt.value = string.gsub(clink.prompt.value, "{env}", color.set_color(color.GREEN,color.BLACK, color.BOLD)..original_prompt_env)
+    clink.prompt.value = string.gsub(clink.prompt.value, "{env}", colored_text(original_prompt_env, color.GREEN, color.BLACK, color.BOLD))
 end
 
 -- Adapted from 'npm.lua'
@@ -186,7 +185,7 @@ local function npm_prompt_filter()
         
         local package_name = package.name or "<no name>"
         local package_version = package.version and "@"..package.version or ""
-        return color.set_color(color.YELLOW,color.BLACK).."("..package_name..package_version..")"
+        return colored_text("("..package_name..package_version..")", color.YELLOW, color.BLACK)
     end
     
     clink.prompt.value = clink.prompt.value:gsub('{npm}', npm_substitute_builder)
@@ -197,7 +196,6 @@ function agnoster_filter()
     local COLOR_PATTERN = "(\x1b[^m]-m)"
     local BACK_COLOR_PATTERN = "4(%d)"
     local RESET_COLOR_PATTERN = "(0)m"
-    local RESET_SEQ = "\x1b[0m"
     local current_back_color = nil
     local function arrow_inserter(current_seq)
         local new_back_color = tonumber(string.match(current_seq, BACK_COLOR_PATTERN) or string.match(current_seq, RESET_COLOR_PATTERN))
@@ -207,7 +205,7 @@ function agnoster_filter()
         
         local substitute_text = current_seq
         if new_back_color ~= current_back_color then
-            substitute_text = color.set_color(current_back_color, new_back_color)..ARROW_SYMBOL.." "..substitute_text
+            substitute_text = colored_text(ARROW_SYMBOL.." "..substitute_text, current_back_color, new_back_color)
             current_back_color = new_back_color
         end
         
